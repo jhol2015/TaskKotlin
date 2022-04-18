6e51d6d39ff2c08c4e3bc865455f6e0422f7e40a
Jhol <galilleu2011@gmail.com>
2022-04-18 14:49:57 -0300

    Create function to splashActivity page :construction:

--- app/src/main/java/com/jholdev/taskkotlin/ui/SplashFragment.kt
+++ app/src/main/java/com/jholdev/taskkotlin/ui/SplashFragment.kt
@@ -1,21 +1,46 @@
 package com.jholdev.taskkotlin.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.jholdev.taskkotlin.R
+import com.jholdev.taskkotlin.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    //Inicia a splash com um delay de 2 segundos
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(this::checkPermissions, 2000)
+    }
+
+
+    //Verifica se usuaro esta logado ou não
+    private fun checkPermissions() {
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
